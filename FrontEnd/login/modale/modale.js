107ba1ca9f167a8b7765c29beb81dd113e8605d1
dolly.azur <azurgrisby@hotmail.fr>
2024-10-11 11:51:33 +0200

    mise en place du window event listener pour escape et tab, o+ const focusable selector

--- FrontEnd/login/modale/modale.js
+++ FrontEnd/login/modale/modale.js
@@ -1,12 +1,14 @@
 let modal = null;
+const focusableSeletor = "button, a, input, textarea";
+let focusables = [];
 
 const openModal = function (e) {
   e.preventDefault();
-  const target = document.querySelector(e.target.getAttribute("href"));
-  target.style.display = null;
-  target.removeAttribute("aria-hidden");
-  target.setAttribute("aria-modal", "true");
-  modal = target;
+  modal = document.querySelector(e.target.getAttribute("href"));
+  focusables = Array.from(modal.querySelectorAll(focusableSeletor));
+  modal.style.display = null;
+  modal.removeAttribute("aria-hidden");
+  modal.setAttribute("aria-modal", "true");
   modal.addEventListener("click", closeModal);
   modal.querySelector(".js-modal-close").addEventListener("click", closeModal);
   modal
@@ -24,12 +26,29 @@ const closeModal = function (e) {
   modal
     .querySelector(".js-modal-close")
     .removeEventListener("click", closeModal);
+  modal
+    .querySelector(".js-modal-stop")
+    .removeEventListener("click", stopPropagation);
   modal = null;
 };
 
 const stopPropagation = function (e) {
   e.stopPropagation();
 };
+
+const focusInModal = function (e) {
+  e.preventDefault();
+};
+
 document.querySelectorAll(".js-modal").forEach((a) => {
   a.addEventListener("click", openModal);
+});
+
+window.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" || e.key === "Esc") {
+    closeModal(e);
+  }
+  if (e.key === "Tab" && modal !== null) {
+    focusInModal(e);
+  }
 });
